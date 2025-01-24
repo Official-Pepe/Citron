4a435ff884a76f5c3295afa88b36976689445e7a
Pepe-manager <milothomas690@proton.me>
2025-01-24 16:05:22 +0100

    Update plant_watering.sh

--- plant_watering.sh
+++ plant_watering.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 java -cp /path/to/pi4j-core.jar:/path/to/your/code PlantWateringSystem &
 
-# Überprüfen, ob Internet verbunden ist
+# Check if Internet is connected
 while ! ping -c 1 -W 1 8.8.8.8 > /dev/null; do
-    echo "Warte auf Internetverbindung..."
+    echo “Waiting for internet connection...”
     sleep 5
 done
-echo "Internet verbunden!"
+echo “Internet connected!”
